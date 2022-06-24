645168ee1440fff224f71ccdb915b7735fc0cab4
Rui Guo <guojiarui@gmail.com>
2022-06-24 01:31:50 -0700

    clean up code, backend mark as completed.

--- modules/movies.js
+++ modules/movies.js
@@ -1,22 +1,34 @@
 'use strict';
 
 const axios = require('axios');
+require('dotenv').config();
 
-
-async function getMovies (req, res) {
+async function getMovies(request, response) {
   let city = request.query.searchQuery;
-    try {
-      let url=`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${city}`;
-      let dataToGroom = await axios.get(url);
-      console.log(dataToGroom);
-      let dataToSend = dataToGroom.data.results.map(movieData => new Movie(movieData));
-      response.status(200).send(dataToSend);
-  
-    } catch (error) {
-     console.log(error.message);
-    }
-  
+
+  try {
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${city}`;
+    let dataToGroom = await axios.get(url);
+    let dataToSend = dataToGroom.data.results.map(movieData => new Movie(movieData));
+    response.status(200).send(dataToSend);
+
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+class Movie {
+  constructor(movieObj) {
+    this.title = movieObj.title;
+    this.overview = movieObj.overview;
+    this.average_votes = movieObj.vote_average;
+    this.image_url = 'https://image.tmdb.org/t/p/w500' + movieObj.poster_path;
+    this.popularity = movieObj.popularity;
+    this.released_on = movieObj.release_date;
+  }
 }
+
+module.exports = getMovies;
+
 // function getMovies(req, res, next) {
 
 //   let city = request.query.searchQuery;
@@ -32,16 +44,3 @@ async function getMovies (req, res) {
 //     .then(dataToSend => response.status(200).send(dataToSend))
 //     .catch(error => console.log(error))
 // };
-
-
-class Movie {
-  constructor(movieObj) {
-    this.title = movieObj.title;
-    this.overview = movieObj.overview;
-    this.average_votes = movieObj.vote_average;
-    this.image_url = 'https://image.tmdb.org/t/p/w500' + movieObj.poster_path;
-    this.popularity = movieObj.popularity;
-    this.released_on = movieObj.release_date;
-  }
-}
-module.exports = getMovies;
